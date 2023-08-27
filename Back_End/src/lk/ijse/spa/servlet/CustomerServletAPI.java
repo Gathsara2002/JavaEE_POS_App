fb92e56a7dd0325345c6279c2fda0e2f668cf249
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-27 15:57:15 +0530

    do option method is override

--- Back_End/src/lk/ijse/spa/servlet/CustomerServletAPI.java
+++ Back_End/src/lk/ijse/spa/servlet/CustomerServletAPI.java
@@ -157,4 +157,12 @@ public class CustomerServletAPI extends HttpServlet {
             resp.getWriter().print(builder.build());
         }
     }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "DELETE");
+        resp.addHeader("Access-Control-Allow-Methods", "PUT");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    }
 }
