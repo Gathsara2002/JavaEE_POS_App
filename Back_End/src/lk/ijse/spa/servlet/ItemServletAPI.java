c7fe04a623517e6e94d1f065b45c4651e1d9c5df
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-27 13:10:07 +0530

    create doGet method in ItemServletAPI

--- Back_End/src/lk/ijse/spa/servlet/ItemServletAPI.java
+++ Back_End/src/lk/ijse/spa/servlet/ItemServletAPI.java
@@ -5,7 +5,52 @@
 
 package lk.ijse.spa.servlet;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
 
+@WebServlet(urlPatterns = "/item")
 public class ItemServletAPI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/webPos", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Item");
+            ResultSet resultSet = pstm.executeQuery();
+
+            resp.addHeader("Content-Type", "application/json");
+
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+
+            while (resultSet.next()) {
+                String code = resultSet.getString(1);
+                String name = resultSet.getString(2);
+                double price = resultSet.getDouble(3);
+                double qty = resultSet.getDouble(4);
+
+                JsonObjectBuilder itemObj = Json.createObjectBuilder();
+                itemObj.add("code", code);
+                itemObj.add("name", name);
+                itemObj.add("price", price);
+                itemObj.add("qty", qty);
+
+                allItems.add(itemObj.build());
+            }
+
+            resp.getWriter().print(allItems.build());
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
