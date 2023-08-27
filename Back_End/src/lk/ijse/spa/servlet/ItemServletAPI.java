b4e964a56fd43575b35eff505192bd5aaf8f55d1
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-27 13:08:38 +0530

    create ItemServletAPI

---
+++ Back_End/src/lk/ijse/spa/servlet/ItemServletAPI.java
@@ -0,0 +1,11 @@
+/**
+ * @author : Gathsara
+ * created : 8/27/2023 -- 1:08 PM
+ **/
+
+package lk.ijse.spa.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class ItemServletAPI extends HttpServlet {
+}
