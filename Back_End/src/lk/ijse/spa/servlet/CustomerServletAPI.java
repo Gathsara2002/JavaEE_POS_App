ca672dbc3cbb17c3fffde91b7f8ace9bcc81c571
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-27 12:59:28 +0530

    complete doPost method in CustomerServletAPI

--- Back_End/src/lk/ijse/spa/servlet/CustomerServletAPI.java
+++ Back_End/src/lk/ijse/spa/servlet/CustomerServletAPI.java
@@ -50,6 +50,37 @@ public class CustomerServletAPI extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("cusId");
+        String name = req.getParameter("cusName");
+        String address = req.getParameter("cusAddress");
+        String tp = req.getParameter("cusTp");
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/webPos", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("insert into Customer values(?,?,?,?)");
+            pstm.setObject(1, id);
+            pstm.setObject(2, name);
+            pstm.setObject(3, address);
+            pstm.setObject(4, tp);
+            boolean isAdded = pstm.executeUpdate() > 0;
+
+            if (isAdded) {
+                System.out.println("customer added successfully");
+                JsonObjectBuilder builder = Json.createObjectBuilder();
+                builder.add("state", "ok");
+                builder.add("message", "Successfully added !");
+                builder.add("data", "");
+                resp.getWriter().print(builder.build());
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+            builder.add("state", "Error");
+            builder.add("message", e.getLocalizedMessage());
+            builder.add("data", "");
+            resp.setStatus(500);
+            resp.getWriter().print(builder.build());
+        }
     }
 }
