9ad82f8c95d00dd42d9ba40310afa51f9d560093
Gathsara2002 <liviru2002mmg@gmail.com>
2023-08-27 19:48:28 +0530

    create placeOrderServletAPI

---
+++ Back_End/src/lk/ijse/spa/servlet/PlaceOrderServletAPI.java
@@ -0,0 +1,26 @@
+/**
+ * @author : Gathsara
+ * created : 8/27/2023 -- 7:47 PM
+ **/
+
+package lk.ijse.spa.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/order")
+public class PlaceOrderServletAPI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
